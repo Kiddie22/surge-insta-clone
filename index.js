cbfe79e7a9731e40dbacdfa6ffc8a30bd7f9d4b9
Kiddie22 <rasula.20191118@iit.ac.lk>
2023-02-04 11:38:41 +0530

    remove helmet

--- index.js
+++ index.js
@@ -1,31 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const helmet = require('helmet');
 const path = require('path');
 require('dotenv').config();
 require('express-async-errors');
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.setHeader(
-    'Content-Security-Policy',
-    "default-src * 'unsafe-inline' 'unsafe-eval';"
-  );
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader(
+//     'Content-Security-Policy',
+//     "default-src * 'unsafe-inline' 'unsafe-eval';"
+//   );
+//   next();
+// });
 
 //MIDDLEWARE
 app.use(express.json());
 app.use(cors());
-app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
 
-app.use(express.static(path.resolve(__dirname, './client/build')));
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/client/build')));
+}
 
 mongoose.set('strictQuery', true);
 mongoose.connect(process.env.MONGO_URI, (err) => {
