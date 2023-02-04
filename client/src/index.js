038def34d481d2ba53c639a3d613d5b81636262d
Kiddie22 <rasula.20191118@iit.ac.lk>
2023-02-04 12:31:27 +0530

    remove recaptcha

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import ReCAPTCHA from 'react-google-recaptcha';
-import env from 'react-dotenv';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
@@ -14,15 +12,9 @@ const store = configureStore({
   reducer: authReducer,
 });
 
-// const onChange = (value) => {
-//   console.log('Captcha value:', value);
-// };
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <ReCAPTCHA sitekey={env.RECAPTCHA} onChange={onChange}>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  // {/* </ReCAPTCHA> */}
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
